c59662fe7aea81e4feffe5f1db024947d54cfc61
Keith Frederick <keithfrederick@Keiths-iMac.local>
2025-03-24 17:58:02 -0500

    Update names on site

--- pages/contact.js
+++ pages/contact.js
@@ -9,7 +9,7 @@ const contact = () => {
   return (
     <>
       <Head>
-        <title>Contact - 7Creative</title>
+        <title>Contact - Gamutstud.io</title>
       </Head>
       <Container>
       <CssBaseline />
