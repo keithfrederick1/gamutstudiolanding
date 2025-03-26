d95bfa15f4beb478141d5c46037499cce388b1e3
Keith Frederick <keithfrederick@Keiths-iMac.local>
2025-03-25 19:33:25 -0500

    Update hero spacing

--- sections/Hero.js
+++ sections/Hero.js
@@ -41,23 +41,28 @@ const Hero = () => {
           </div>
         </div>
       </section>
-      <section className='hero-sec'>
-        <div className='container'>
-          <div className='heading-title'>
-            <Title title="No...Seriously. It's 2025." />
-            <p style={{ 'fontSize': '24px'}}>At Gamut Studio, we build badass, high-performance digital solutions that turn clicks into conversions and ideas into revenue. Scalable tech, seamless UX, and straight-up results—because slow and clunky doesn’t cut it Let’s make your business move fast and break limits.
-           </p> </div>
-          <div className='hero-content grid-4'>
-            {home.map((item, i) => (
-              <div className='box' key={i}>
-                <span className='green'>{item.icon}</span> <br />
-                <br />
-                <h3>{item.title}</h3>
-              </div>
-            ))}
-          </div>
-        </div>
+      <section>
+        
       </section>
+      <section className='hero-sec'>
+  <div className='container'>
+    <div className='heading-title'>
+      <Title title="No...Seriously. It's 2025." />
+      <p style={{ fontSize: '24px' }}>
+        At Gamut Studio, we build badass, high-performance digital solutions that turn clicks into conversions and ideas into revenue. Scalable tech, seamless UX, and straight-up results—because slow and clunky doesn’t cut it. Let’s make your business move fast and break limits.
+      </p>
+    </div>
+    <div className='hero-content grid-4'>
+      {home.map((item, i) => (
+        <div className='box' key={i}>
+          <span className='green'>{item.icon}</span> <br />
+          <br />
+          <h3>{item.title}</h3>
+        </div>
+      ))}
+    </div>
+  </div>
+</section>
       <ShowCase />
       <Banner />
       <Testimonial />
